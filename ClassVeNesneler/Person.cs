8413733076ab29205aad23f7a49f6803416ac8ec
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-11 19:05:40 +0300

    Add project files.

---
+++ ClassVeNesneler/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVeNesneler
+{
+    internal class Person
+    {
+        public string ad;
+        public string soyad;
+        public DateTime dogumTarihi;
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        public void KisiBilgisi()
+        {
+            Console.WriteLine($"Adı: {Ad}");
+            Console.WriteLine($"Soyadı: {Soyad}");
+            Console.WriteLine($"Doğum Tarihi: {DogumTarihi}");
+            return;
+        }
+
+    }
+}
---
+++ ClassVeNesneler/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassVeNesneler;
+
+Person kisi1 = new Person();
+kisi1.Ad = "Emre";
+kisi1.Soyad = "Şekerci";
+kisi1.DogumTarihi = new DateTime(1999, 1, 18); 
+
+Person kisi2 = new Person();
+kisi2.Ad = "Ahmet";
+kisi2.Soyad = "Demir";
+kisi2.DogumTarihi = new DateTime(1979, 4, 27); 
+
+Person kisi3 = new Person();
+kisi3.Ad = "Cansu";
+kisi3.Soyad = "Taşdan";
+kisi3.DogumTarihi = new DateTime(1997, 3, 4); 
+
+kisi1.KisiBilgisi();
+kisi2.KisiBilgisi();
+kisi3.KisiBilgisi();
